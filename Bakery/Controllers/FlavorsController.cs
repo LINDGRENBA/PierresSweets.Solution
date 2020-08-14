eefc3df5de50813362545b306be2ede6abc003dd
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-08-14 15:58:56 -0700

    Add initial routes for index and create to flavorscontroller.

--- Bakery/Controllers/FlavorsController.cs
+++ Bakery/Controllers/FlavorsController.cs
@@ -1,28 +1,45 @@
-// using Microsoft.AspNetCore.Mvc.Rendering;
-// using Microsoft.AspNetCore.Authorization;
-// using Microsoft.AspNetCore.Identity;
-// using Microsoft.EntityFrameworkCore;
-// using System.Collections.Generic;
-// using Microsoft.AspNetCore.Mvc;
-// using System.Threading.Tasks; 
-// using System.Security.Claims;
-// using Bakery.ViewModels;
-// using Bakery.Models;
-// using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks; 
+using System.Security.Claims;
+using Bakery.ViewModels;
+using Bakery.Models;
+using System.Linq;
 
-// namespace Bakery.Controllers
-// {
-//   public class FlavorsController : Controller
-//   {
-//     private readonly BakeryContext _db;
-//     private readonly UserManager<ApplicationUser> userManager;
+namespace Bakery.Controllers
+{
+  [Authorize]
+  public class FlavorsController : Controller
+  {
+    private readonly BakeryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-//     public FlavorsController(UserManager<ApplicationUser> userManager, BakeryContext db)
-//     {
-//       _userManager = userManager;
-//       _db = db;
-//     }
+    public FlavorsController(UserManager<ApplicationUser> userManager, BakeryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Flavors.OrderBy(flavors => flavors.Type).ToList());
+    }
     
+    public ActionResult Create()
+    {
+      return View();
+    }
     
-//   }
-// }
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
