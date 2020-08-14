dc1535c8f474c3f30d088a0c4c679666e3e943df
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-08-14 08:49:56 -0700

    Add initial Model directories and files with some initial configuration.

---
+++ Bakery/Models/BakeryContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bakery.Models
+{
+  public class BakeryContext : IdentityDbContext<ApplicationUser> //tells identity which class in the application will contain the user account information identity is responsible for authenticating
+  {
+    public virtual DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat {get; set; }
+    
+    public BakeryContext(DbContextOptions options) : base(options) { }
+  }
+}
