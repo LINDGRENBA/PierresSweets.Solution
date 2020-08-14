2ec95eb3960acd2135220b0008096898bf70e0f8
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-08-14 09:10:52 -0700

    Add model for FlavorTreat join table.

---
+++ Bakery/Models/FlavorTreat.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System;
+
+namespace Bakery.Models
+{
+  public class FlavorTreat
+  {
+    public int FlavorTreatId { get; set; }
+    public int FlavorId { get; set; }
+    public Flavor Flavor { get; set; }
+    public int TreatId { get; set; }
+    public Treat Treat { get; set; }
+  }
+}
